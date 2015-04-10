1ee9b49e0785ff162543e7f550d9dbd257712a15
Jake Siegers <sirtopeia@yahoo.com>
2015-04-09 20:25:54 -0500

    Minor Changes

--- js/visualizer.js
+++ js/visualizer.js
@@ -1,6 +1,7 @@
 var canvas;
 var canvasCtx;
 var sinCount = 0;
+var ratio
 
 $(function(){
 	vis_initVisualizer();
@@ -13,6 +14,7 @@ function vis_initVisualizer(){
 	canvasCtx = canvas.getContext("2d");
 	canvasCtx.fillStyle = '#202020';
 	canvasCtx.fillRect(0, 0, canvas.width,  canvas.height);
+	ratio = vis_getScreenWidth()/vis_getScreenHeight();
 	setInterval(vis_draw,100);
 }
 
@@ -37,25 +39,25 @@ function vis_draw(){
 	canvasCtx.fillStyle = '#202020';
 	canvasCtx.fillRect(0, 0, canvas.width,  canvas.height);
 
-	sinCount+=0.005;
+	sinCount+=0.01;
 	if(sinCount > Math.PI*2){
 		sinCount = 0;
 	}
-	
-	for(var i=0;i<40;i++){
+
+	for(var i=0;i<20;i++){
 		if(i%2==0){
-			canvasCtx.fillStyle = 'rgba(29,144,153,0.5)';
+			canvasCtx.fillStyle = 'rgba(29,144,153,0.3)';
 		}else{
-			canvasCtx.fillStyle = 'rgba(213,58,51,0.5)';
+			canvasCtx.fillStyle = 'rgba(213,58,51,0.3)';
 		}
-		vis_drawRect(((canvas.width/40)*i)+Math.sin(sinCount)*50,(canvas.height/2)+Math.sin(sinCount*(i+1))*(canvas.height/2));
+		vis_drawRect((canvas.width/20)*i,(canvas.height/2)+Math.sin(sinCount*(i+1))*(canvas.height/2));
 	}
-	
+
 }
 
 function vis_drawRect(x,y){
-	var gridSizeX = 40
-	var gridSizeY = 40
+	var gridSizeX = 80;
+	var gridSizeY = 80;
 	var gridOffsetX = gridSizeX/2;
 	var gridOffsetY = gridSizeY/2;
 	var ax=gridSizeX * Math.floor((x + gridOffsetX) / gridSizeX);
